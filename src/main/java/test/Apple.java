7dbc0d7c928a04fd41100f0f2269b127ae0dc931
kyocoolcool <kyocoolcool@hotmail.com>
2019-04-12 00:00:49 +0800

    edit

--- src/main/java/test/Apple.java
+++ src/main/java/test/Apple.java
@@ -9,7 +9,7 @@ package test;
  **/
 
 public class Apple {
-    private String appName="Good777";
+    private String appName="Hello World Jenkins";
 
     public Apple(String appName) {
         this.appName = appName;
